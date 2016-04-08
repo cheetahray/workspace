18d11f3e4e4ad3098fc3e90f15dc2bb43a14a2a9
cheetahray <ray608@gmail.com>
2016-04-09 04:41:03 +0800

    Do 完成了

--- bellclass/www/js/index.js
+++ bellclass/www/js/index.js
@@ -1,4 +1,3 @@
-
 var app = {
     // Application Constructor
     initialize: function() {
@@ -17,14 +16,32 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         var success = function(message) {
-	        alert(message);
+	        console.log(message);
 	    }
 	
 	    var failure = function(err) {
-	        alert(err);
+	        console.log(err);
 	    }
 
         hello.initialize("192.168.11.40", 12345, success, failure);
+		
+		var box1 = document.getElementById('DO');
+    
+		box1.addEventListener('touchstart', function(e){
+			//var touchobj = e.changedTouches[0]; // reference first touch point (ie: first finger)
+			//startx = parseInt(touchobj.clientX); // get x position of touch point relative to left edge of browser
+			hello.sendMessage("DO", success2, failure2);
+			box1.setAttribute('style', 'background-color:#B6221E;');
+			e.preventDefault();
+		}, false);
+		
+		box1.addEventListener('touchend', function(e){
+			//var touchobj = e.changedTouches[0]; // reference first touch point (ie: first finger)
+			//startx = parseInt(touchobj.clientX); // get x position of touch point relative to left edge of browser
+			box1.setAttribute('style', 'background-color:transparent;');
+			e.preventDefault();
+		}, false);
+		
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
@@ -37,8 +54,17 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-        
+        //hello.sendMessage("Fa", success2, failure2)
     }
+	
 };
 
+var success2 = function(message) {
+    console.log(message);
+}
+	
+var failure2 = function(err) {
+	console.log(err);
+}
+        
 app.initialize();
