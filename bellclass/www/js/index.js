7a19f37a965a203dcecdb7ccd86d3b0a1d9f3e8f
ray <ray@dac.tw>
2016-04-15 21:00:51 +0800

    不得不如此

--- bellclass/www/js/index.js
+++ bellclass/www/js/index.js
@@ -59,6 +59,186 @@ var failure = function (err) {
     navigator.notification.alert("網路設定失敗" + err, alertDismissed, '', '確定');
 }
 
+var boxRec = document.getElementById('REC');
+
+boxRec.addEventListener('touchstart', function (e) {
+    //var touchobj = e.changedTouches[0]; // reference first touch point (ie: first finger)
+    //startx = parseInt(touchobj.clientX); // get x position of touch point relative to left edge of browser
+    if (document.getElementById("already").value == "0")
+        readyet();
+    else if (document.getElementById("record").value == "0") {
+        boxRec.setAttribute('style', 'background: url(img/2-iphone-layout_iphone.png);');
+        sendto("R", finalcountdown.toString(), myssid + entry);
+        document.getElementById("record").value = "1";
+        finalcountdown = 510;
+    }
+    else {
+        finalboxRec();
+    }
+    e.preventDefault();
+}, false);
+
+var finalboxRec = function () {
+    document.getElementById("record").value = "0";
+    boxRec.setAttribute('style', 'background: none;');
+    sendto("Q", finalcountdown.toString(), myssid + entry);
+    $('#runner').runner('reset', true);
+}
+
+var boxDoo = document.getElementById('DOO');
+
+boxDoo.addEventListener('touchstart', function (e) {
+    readyet();
+    if (document.getElementById("already").value == "1")
+        boxDoo.setAttribute('style', 'background-color:#B6221E;');
+    e.preventDefault();
+}, false);
+
+boxDoo.addEventListener('touchend', function (e) {
+    if (document.getElementById("already").value == "1")
+        boxDoo.setAttribute('style', 'background-color:transparent;');
+    sendto("239", finalcountdown.toString(), myssid + entry);
+    e.preventDefault();
+}, false);
+
+var boxTi = document.getElementById('TI');
+
+boxTi.addEventListener('touchstart', function (e) {
+    readyet();
+    if (document.getElementById("already").value == "1")
+        boxTi.setAttribute('style', 'background-color:#CA27D3;');
+    e.preventDefault();
+}, false);
+
+boxTi.addEventListener('touchend', function (e) {
+    if (document.getElementById("already").value == "1")
+        boxTi.setAttribute('style', 'background-color:transparent;');
+    sendto("223", finalcountdown.toString(), myssid + entry);
+    e.preventDefault();
+}, false);
+
+var boxLa = document.getElementById('LA');
+
+boxLa.addEventListener('touchstart', function (e) {
+    readyet();
+    if (document.getElementById("already").value == "1")
+        boxLa.setAttribute('style', 'background-color:#233AB9;');
+    e.preventDefault();
+}, false);
+
+boxLa.addEventListener('touchend', function (e) {
+    if (document.getElementById("already").value == "1")
+        boxLa.setAttribute('style', 'background-color:transparent;');
+    sendto("207", finalcountdown.toString(), myssid + entry);
+    e.preventDefault();
+}, false);
+
+var boxSo = document.getElementById('SO');
+
+boxSo.addEventListener('touchstart', function (e) {
+    readyet();
+    if (document.getElementById("already").value == "1")
+        boxSo.setAttribute('style', 'background-color:#2CC8C7;');
+    e.preventDefault();
+}, false);
+
+boxSo.addEventListener('touchend', function (e) {
+    if (document.getElementById("already").value == "1")
+        boxSo.setAttribute('style', 'background-color:transparent;');
+    sendto("191", finalcountdown.toString(), myssid + entry);
+    e.preventDefault();
+}, false);
+
+var boxFa = document.getElementById('FA');
+
+boxFa.addEventListener('touchstart', function (e) {
+    readyet();
+    if (document.getElementById("already").value == "1")
+        boxFa.setAttribute('style', 'background-color:#3CD12F;');
+    e.preventDefault();
+}, false);
+
+boxFa.addEventListener('touchend', function (e) {
+    if (document.getElementById("already").value == "1")
+        boxFa.setAttribute('style', 'background-color:transparent;');
+    sendto("175", finalcountdown.toString(), myssid + entry);
+    e.preventDefault();
+}, false);
+
+var boxMi = document.getElementById('MI');
+
+boxMi.addEventListener('touchstart', function (e) {
+    readyet();
+    if (document.getElementById("already").value == "1")
+        boxMi.setAttribute('style', 'background-color:#D1D62E;');
+    e.preventDefault();
+}, false);
+
+boxMi.addEventListener('touchend', function (e) {
+    if (document.getElementById("already").value == "1")
+        boxMi.setAttribute('style', 'background-color:transparent;');
+    sendto("159", finalcountdown.toString(), myssid + entry);
+    e.preventDefault();
+}, false);
+
+var boxRe = document.getElementById('RE');
+
+boxRe.addEventListener('touchstart', function (e) {
+    readyet();
+    if (document.getElementById("already").value == "1")
+        boxRe.setAttribute('style', 'background-color:#D5922E;');
+    e.preventDefault();
+}, false);
+
+boxRe.addEventListener('touchend', function (e) {
+    if (document.getElementById("already").value == "1")
+        boxRe.setAttribute('style', 'background-color:transparent;');
+    sendto("143", finalcountdown.toString(), myssid + entry);
+    e.preventDefault();
+}, false);
+
+var boxDo = document.getElementById('DO');
+
+boxDo.addEventListener('touchstart', function (e) {
+    readyet();
+    if (document.getElementById("already").value == "1")
+        boxDo.setAttribute('style', 'background-color:#BE2E2B;');
+    e.preventDefault();
+}, false);
+
+boxDo.addEventListener('touchend', function (e) {
+    if (document.getElementById("already").value == "1")
+        boxDo.setAttribute('style', 'background-color:transparent;');
+    sendto("127", finalcountdown.toString(), myssid + entry);
+    e.preventDefault();
+}, false);
+
+var boxSet = document.getElementById('SET');
+
+boxSet.addEventListener('touchstart', function (e) {
+    if (document.getElementById("record").value == "1")
+        finalboxRec();
+    WifiWizard.getCurrentSSID(successid, failure);
+    e.preventDefault();
+}, false);
+
+// example of a callback method
+var successCallback = function (result) {
+    if (result.type === 'sleep') {
+        finalboxRec();
+        navigator.notification.alert("不好意思板子的記憶體有限", alertDismissed, '', '確定');
+    } else if (result.type === 'countdown') {
+        console.log('time until sleep: ' + result.timeLeft + ' seconds');
+    } else {
+        console.log('unhandled type (' + result.type + ')');
+    }
+};
+
+// example of a callback method
+var errorCallback = function (error) {
+    navigator.notification.alert(error, alertDismissed, '', '確定');
+};
+
 var app = {
     // Application Constructor
     initialize: function () {
@@ -88,186 +268,6 @@ var app = {
         var ratio = app.deviceHeight / app.deviceWidth;
         if (ratio > 1.55 && window.location.href.indexOf("index") > 0)
             window.location.assign("second.html");
-
-        var boxRec = document.getElementById('REC');
-
-        boxRec.addEventListener('touchstart', function (e) {
-            //var touchobj = e.changedTouches[0]; // reference first touch point (ie: first finger)
-            //startx = parseInt(touchobj.clientX); // get x position of touch point relative to left edge of browser
-            if (document.getElementById("already").value == "0")
-                readyet();
-            else if (document.getElementById("record").value == "0") {
-                boxRec.setAttribute('style', 'background: url(img/2-iphone-layout_iphone.png);');
-                sendto("R", finalcountdown.toString(), myssid + entry);
-                document.getElementById("record").value = "1";
-                finalcountdown = 510;
-            }
-            else {
-                finalboxRec();
-            }
-            e.preventDefault();
-        }, false);
-
-        var finalboxRec = function () {
-            document.getElementById("record").value = "0";
-            boxRec.setAttribute('style', 'background: none;');
-            sendto("Q", finalcountdown.toString(), myssid + entry);
-            $('#runner').runner('reset', true);
-        }
-
-        var boxDoo = document.getElementById('DOO');
-
-        boxDoo.addEventListener('touchstart', function (e) {
-            readyet();
-            if (document.getElementById("already").value == "1")
-                boxDoo.setAttribute('style', 'background-color:#B6221E;');
-            e.preventDefault();
-        }, false);
-
-        boxDoo.addEventListener('touchend', function (e) {
-            if (document.getElementById("already").value == "1")
-                boxDoo.setAttribute('style', 'background-color:transparent;');
-            sendto("239", finalcountdown.toString(), myssid + entry);
-            e.preventDefault();
-        }, false);
-
-        var boxTi = document.getElementById('TI');
-
-        boxTi.addEventListener('touchstart', function (e) {
-            readyet();
-            if (document.getElementById("already").value == "1")
-                boxTi.setAttribute('style', 'background-color:#CA27D3;');
-            e.preventDefault();
-        }, false);
-
-        boxTi.addEventListener('touchend', function (e) {
-            if (document.getElementById("already").value == "1")
-                boxTi.setAttribute('style', 'background-color:transparent;');
-            sendto("223", finalcountdown.toString(), myssid + entry);
-            e.preventDefault();
-        }, false);
-
-        var boxLa = document.getElementById('LA');
-
-        boxLa.addEventListener('touchstart', function (e) {
-            readyet();
-            if (document.getElementById("already").value == "1")
-                boxLa.setAttribute('style', 'background-color:#233AB9;');
-            e.preventDefault();
-        }, false);
-
-        boxLa.addEventListener('touchend', function (e) {
-            if (document.getElementById("already").value == "1")
-                boxLa.setAttribute('style', 'background-color:transparent;');
-            sendto("207", finalcountdown.toString(), myssid + entry);
-            e.preventDefault();
-        }, false);
-
-        var boxSo = document.getElementById('SO');
-
-        boxSo.addEventListener('touchstart', function (e) {
-            readyet();
-            if (document.getElementById("already").value == "1")
-                boxSo.setAttribute('style', 'background-color:#2CC8C7;');
-            e.preventDefault();
-        }, false);
-
-        boxSo.addEventListener('touchend', function (e) {
-            if (document.getElementById("already").value == "1")
-                boxSo.setAttribute('style', 'background-color:transparent;');
-            sendto("191", finalcountdown.toString(), myssid + entry);
-            e.preventDefault();
-        }, false);
-
-        var boxFa = document.getElementById('FA');
-
-        boxFa.addEventListener('touchstart', function (e) {
-            readyet();
-            if (document.getElementById("already").value == "1")
-                boxFa.setAttribute('style', 'background-color:#3CD12F;');
-            e.preventDefault();
-        }, false);
-
-        boxFa.addEventListener('touchend', function (e) {
-            if (document.getElementById("already").value == "1")
-                boxFa.setAttribute('style', 'background-color:transparent;');
-            sendto("175", finalcountdown.toString(), myssid + entry);
-            e.preventDefault();
-        }, false);
-
-        var boxMi = document.getElementById('MI');
-
-        boxMi.addEventListener('touchstart', function (e) {
-            readyet();
-            if (document.getElementById("already").value == "1")
-                boxMi.setAttribute('style', 'background-color:#D1D62E;');
-            e.preventDefault();
-        }, false);
-
-        boxMi.addEventListener('touchend', function (e) {
-            if (document.getElementById("already").value == "1")
-                boxMi.setAttribute('style', 'background-color:transparent;');
-            sendto("159", finalcountdown.toString(), myssid + entry);
-            e.preventDefault();
-        }, false);
-
-        var boxRe = document.getElementById('RE');
-
-        boxRe.addEventListener('touchstart', function (e) {
-            readyet();
-            if (document.getElementById("already").value == "1")
-                boxRe.setAttribute('style', 'background-color:#D5922E;');
-            e.preventDefault();
-        }, false);
-
-        boxRe.addEventListener('touchend', function (e) {
-            if (document.getElementById("already").value == "1")
-                boxRe.setAttribute('style', 'background-color:transparent;');
-            sendto("143", finalcountdown.toString(), myssid + entry);
-            e.preventDefault();
-        }, false);
-
-        var boxDo = document.getElementById('DO');
-
-        boxDo.addEventListener('touchstart', function (e) {
-            readyet();
-            if (document.getElementById("already").value == "1")
-                boxDo.setAttribute('style', 'background-color:#BE2E2B;');
-            e.preventDefault();
-        }, false);
-
-        boxDo.addEventListener('touchend', function (e) {
-            if (document.getElementById("already").value == "1")
-                boxDo.setAttribute('style', 'background-color:transparent;');
-            sendto("127", finalcountdown.toString(), myssid + entry);
-            e.preventDefault();
-        }, false);
-
-        var boxSet = document.getElementById('SET');
-
-        boxSet.addEventListener('touchstart', function (e) {
-            if (document.getElementById("record").value == "1")
-                finalboxRec();
-            WifiWizard.getCurrentSSID(successid, failure);
-            e.preventDefault();
-        }, false);
-
-        // example of a callback method
-        var successCallback = function (result) {
-            if (result.type === 'sleep') {
-                finalboxRec();
-                navigator.notification.alert("不好意思板子的記憶體有限", alertDismissed, '', '確定');
-            } else if (result.type === 'countdown') {
-                console.log('time until sleep: ' + result.timeLeft + ' seconds');
-            } else {
-                console.log('unhandled type (' + result.type + ')');
-            }
-        };
-
-        // example of a callback method
-        var errorCallback = function (error) {
-            navigator.notification.alert(error, alertDismissed, '', '確定');
-        };
 
         document.addEventListener("offline", onOffline, false);
         document.addEventListener("online", onOnline, false);
