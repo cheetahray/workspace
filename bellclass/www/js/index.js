1f4e2240221e0867a94c6d5a3c12aa407339a24d
ray <ray@dac.tw>
2016-05-09 19:07:31 +0800

    砍掉 keepscreenon networkinformation

--- bellclass/www/js/index.js
+++ bellclass/www/js/index.js
@@ -333,9 +333,6 @@ var app = {
                 );
             }
         });
-        //document.addEventListener("offline", onOffline, false);
-        //document.addEventListener("online", onOnline, false);
-        keepscreenon.enable();
 
         anyscreen([''], function () { //(['./css/index.css'],function() {
 
@@ -428,30 +425,6 @@ function onPrompt(results) {
             hello.initialize(myssid + entry, 8888, success, failure);
         }
     }
-}
-
-function checkConnection() {
-    var networkState = navigator.connection.type;
-
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
-
-    navigator.notification.alert('Connection type: ' + states[networkState]);
-}
-
-function onOffline() {
-    document.getElementById("already").value = "0";
-}
-
-function onOnline() {
-    document.getElementById("already").value = "0";
 }
 
 app.initialize();
